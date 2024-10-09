e806e1bc172ff8303815f7391130ed054f2d0a29
Guillaume <xgold3nfir3@gmail.com>
2024-10-09 15:33:44 +0200

    tools for user interface

---
+++ src/UI_Tools.py
@@ -0,0 +1,16 @@
+import customtkinter as ctk
+from InstanceManager import *
+
+colors = {'light_blue': '#02B0D5', 'gray': '#323030', 'dark_blue': '#3B536B', 'white': '#D9D9D9', 'pink': '#E33B74',
+          'hlb': '#06D3FF', 'hdb': '#497198', 'hp': '#FE4181', 'disabled': '#D9D9D9', 'hgray': '#272525',
+          'red': '#8F2828', 'green': '#1C8D12'}
+
+
+def center(widget: ctk.CTkBaseClass):
+    widget.grid_rowconfigure(0, weight=1)
+    widget.grid_columnconfigure(0, weight=1)
+
+
+def fastgrid(widget: ctk.CTkBaseClass, x, y, xpad, ypad, sticky, columnspan=1, rowspan=1):
+    widget.grid(row=x, column=y, padx=xpad, pady=ypad, sticky=sticky, columnspan=columnspan, rowspan=rowspan)
+
