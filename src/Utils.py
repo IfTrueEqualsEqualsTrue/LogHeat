b09b6502796cafa7f3abcbbf70b8b4bac71f73d8
Guillaume <xgold3nfir3@gmail.com>
2024-11-13 13:40:25 +0100

    added com port list and toggle button on the webapp interface

--- src/Utils.py
+++ src/Utils.py
@@ -1,7 +1,14 @@
 import serial.tools.list_ports as list_ports
+from flask import jsonify
 
 
 def get_available_com_ports():
     """Returns a list of available COM ports on the computer."""
     ports = list_ports.comports()
     return [port.device for port in ports]
+
+
+def get_com_ports_json():
+    """Returns the available COM ports as a JSON response."""
+    ports = get_available_com_ports()
+    return jsonify(ports=ports)
--- src/WebApp.py
+++ src/WebApp.py
@@ -1,9 +1,10 @@
-import os
+from flask import Flask, render_template, jsonify, request
 
-from flask import Flask, render_template, jsonify
+from Utils import get_com_ports_json
 
 app = Flask(__name__, template_folder='../templates')
 state = False
+current_com_port = None
 
 
 @app.route('/')
@@ -17,6 +18,20 @@ def toggle():
     state = not state
     print(f'State: {state}')
     return jsonify(state=state)
+
+
+@app.route('/update_com_port', methods=['POST'])
+def update_com_port():
+    global current_com_port
+    data = request.get_json()
+    current_com_port = data['port']
+    print(f'COM Port updated to: {current_com_port}')
+    return jsonify(port=current_com_port)
+
+
+@app.route('/get_com_ports', methods=['GET'])
+def get_com_ports():
+    return get_com_ports_json()
 
 
 if __name__ == '__main__':
